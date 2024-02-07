ee834bc8e26b9f7f4de8822ce0668c190f5c13d7
Louise Romero <juvi.romero.up@phinmaed.com>
2024-02-07 11:35:39 +0800

    Rework

--- app/src/main/java/com/example/gymrat/Chestfragment.kt
+++ app/src/main/java/com/example/gymrat/Chestfragment.kt
@@ -6,21 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.gymrat.databinding.FragmentIntro2Binding
+import com.example.gymrat.databinding.FragmentChestfragmentBinding
+import com.example.gymrat.databinding.FragmentHomiesBinding
 
-class Intro2Fragment : Fragment() {
-    private lateinit var binding: FragmentIntro2Binding
+class Chestfragment : Fragment() {
+    private lateinit var binding: FragmentChestfragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentIntro2Binding.inflate(inflater,container,false)
-        binding.next.setOnClickListener {
-            findNavController().navigate(R.id.action_intro2Fragment_to_genderFragment)
-        }
+        binding = FragmentChestfragmentBinding.inflate(inflater,container,false)
         binding.back.setOnClickListener {
-            findNavController().navigate(R.id.action_intro2Fragment_to_intro1Fragment)
+            findNavController().navigate(R.id.action_chestfragment_to_homiesFragment)
         }
         return binding.root
     }
+
 }
